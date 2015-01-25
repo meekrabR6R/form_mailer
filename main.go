fa34dabebae0484164d6695c0a6e086c0065a11b
Nick Miano <nmiano84@gmail.com>
2015-01-25 15:07:30 -0500

    restructure server code

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -12,6 +13,13 @@ func main() {
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 	http.Handle("/", router)
 
-	fmt.Println("Listening on 8000")
-	http.ListenAndServe(":8000", nil)
+	//fmt.Println("Listening on 8000")
+	//http.ListenAndServe(":8000", nil)
+
+	bind := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+	fmt.Printf("listening on %s...", bind)
+	err := http.ListenAndServe(bind, nil)
+	if err != nil {
+		panic(err)
+	}
 }
