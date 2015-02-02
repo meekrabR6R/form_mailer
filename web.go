ee75cc2110a599ae95aaee83f97af969e551cb66
Nick Miano <nmiano84@gmail.com>
2015-02-01 23:31:27 -0500

    config

--- web.go
+++ web.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"code.google.com/p/gofpdf"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 	"os"
 )
+
+/**
+ * Global config
+ */
+type Config struct {
+	Title string
+}
+
+var config = getConf()
 
 func FormHandler(w http.ResponseWriter, req *http.Request) {
 	err := req.ParseForm()
@@ -22,9 +33,30 @@ func FormHandler(w http.ResponseWriter, req *http.Request) {
 		Email:     form["emailAddress"][0],
 		Link:      form["downloadLink"][0],
 	}
-
+	fmt.Println(config.Title)
+	makeAPDF(artist)
 	fmt.Println(extractWorks(form))
 	fmt.Println(artist.FirstName)
+}
+
+func makeAPDF(artist Artist) {
+	pdf := gofpdf.New("P", "mm", "A4", "../font")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, config.Title)
+	err := pdf.OutputFileAndClose("temp1.pdf")
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getConf() (conf *Config) {
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	conf = &Config{}
+	decoder.Decode(&conf)
+	return
 }
 
 func main() {
